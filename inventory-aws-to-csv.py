1c4f441d6d01d255ae67fc63c272b02b49728256
aaron caito <aaron.caito@gmail.com>
2017-02-14 11:43:30 -0500

    +functions, +efs, +ec2

--- inventory-aws-to-csv.py
+++ inventory-aws-to-csv.py
@@ -7,9 +7,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--profile", help="specify aws profile to use")
-parser.add_argument("--volume_size", help="returns volume sizes if true", type=bool)
+parser.add_argument("--s3objects", type=bool, default=False, help="True enables counting s3 objects, default = false")
+parser.add_argument("--volume_size", type=bool, help="returns volume sizes if true")
 parser.add_argument("--region", help="specify region to report on")
-parser.add_argument("--globals", help="returns global data if true", type=bool)
+parser.add_argument("--globals", type=bool, help="returns global data if true")
 args = parser.parse_args()
 profile = args.profile
 
@@ -20,7 +21,16 @@ profile = args.profile
 ## subnets
 
 #compute
-## ec2
+def aws_compute(aws,regions):
+    "This kicks off all compute inventory functions"
+    ec2_instances(aws,regions)
+
+def ec2_instances(aws,regions):
+    "This prints out count of ec2 instances on an account"
+    for region in regions:
+        print("ec2.instances.{}:".format(region),sum(1 for _ in boto3.session.Session(profile_name=profile,region_name=region).resource('ec2').instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])))
+    return
+
 ## ecs
 ## asg
 ## lambda
@@ -29,15 +39,26 @@ profile = args.profile
 
 #storage
 def aws_storage(aws):
+    "This kicks off all storage inventory functions"
+    efs_filesystems(aws)
     s3_buckets(aws)
-    s3_objects(aws)
+    if args.s3objects:
+        s3_objects(aws)
 
 ## ebs
-## efs
-##s3
+
+def efs_filesystems(aws):
+    "This prints count of efs file systems on account"
+    efsclient = aws.client('efs')
+    try:
+        response = efsclient.describe_file_systems()
+        print("efs.filesystem.count:", len(response['FileSystems']))
+    except:
+        print("efs.filesystem.count: none")
+    return
+
 def s3_buckets(aws):
     "This prints a count of buckets on an account"
-    s3 = aws.resource('s3')
     s3client = aws.client('s3')
     try:
         response = s3client.list_buckets()
@@ -76,4 +97,6 @@ def s3_objects(aws):
 
 if __name__ == '__main__':
     aws = boto3.session.Session(profile_name=profile)
+    regions = [region['RegionName'] for region in aws.client('ec2').describe_regions()['Regions']]
     aws_storage(aws)
+    aws_compute(aws,regions)
